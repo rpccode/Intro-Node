3c3fec16062bbc4def9beba1bdef5bddda8e82e7
Rudy Casilla <rpccode@gmail.com>
2021-11-28 11:51:29 -0500

    Iniciando App , Instalando Express y Probandola

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('*', (resque, response) => {
+
+    response.send({ message:'Chanchito feliz'})
+
+})
+
+app.listen(3000, ()=> {console.log('Nuestro servidor esta escuchando en el puerto 3000')})
